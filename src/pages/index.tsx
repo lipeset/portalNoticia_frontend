c259df25ce21020fbcb75de9e7038d78bf5af43c
Felipe <felipea.lucas@outlook.com>
2021-03-05 11:05:51 -0300

    index_config

---
+++ src/pages/index.tsx
@@ -0,0 +1,31 @@
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Piloto({ piloto }) {
+    return (
+        <ul>
+            {piloto.map((pilots) => (
+                <li key={pilots._id}>
+                    <Link href={`/pilotos/${pilots._id}`}>
+                        <a>
+                            {pilots.name}
+                        </a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const response = await fetch('http://localhost:3001/pilots');
+    const data = await response.json();
+
+    return {
+        props: {
+            piloto: data,
+        },
+        revalidate: 10
+    }
+};
