d6211baa832c0cfd82160fff8868176efab89767
Felipe <felipea.lucas@outlook.com>
2021-03-04 23:08:00 -0300

    env_set

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+import { GetStaticProps } from "next";
+
+export default function Home({ org }) {
     return (
-        <h1>Hello world</h1>
+        <div>
+            <h1>{org.fullName}</h1>
+            <h3>{org.teamPolePositions}</h3>
+        </div>
     )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+    const response = await fetch('hhttp://localhost:3001/teams');
+    const data = await response.json();
+
+    return {
+        props: {
+            org: data,
+        },
+        revalidate: 10
+    }
+};
