e704f8cdc765c0ff2a363b882da2ed5bad9fe638
Felipe <felipea.lucas@outlook.com>
2021-03-11 19:36:56 -0300

    races_create

--- src/components/Races/index.tsx
+++ src/components/Races/index.tsx
@@ -2,9 +2,153 @@ import React from 'react';
 
 function Races() {
     return (
-        <div>
-            <h1>teste</h1>
-        </div>
+        <div className="text-white bg-black grid grid-cols-6 justify-center body-font">
+
+            <div className="grid grid-rows-3 grid-flow-col justify-center "> {/*CORRIDA 1*/}
+                <div className="flex flex-wrap content-center justify-center border-r border-white">
+                    <img className="mt-3 flex flex-wrap content-center justify-center"
+                        src="https://static.wixstatic.com/media/af30c3_82b173caa21a411584f9b33a5982edfb~mv2_d_3543_2189_s_2.png/v1/crop/x_0,y_155,w_3543,h_2034/fill/w_148,h_80,al_c,q_85,usm_1.20_1.00_0.01/logo-kart%C3%B3dromo-internacional-impress%C3%A3o_.webp" />
+                    <p className="italic">KARTÓDROMO ALDEIA DA SERRA</p>
+                </div>
+                <div className="flex flex-wrap content-center justify-center border-r border-white">
+                    <img className="mt-3 flex flex-wrap content-center justify-center w-7/12"
+                        src="https://i.imgur.com/7KjJ4Ta.png" />
+                </div>
+                <div className="flex flex-wrap content-center justify-center border-r border-white rounded-b-3xl">
+                    <div className="grid grid-rows-3 grid-flow-col justify-center ">
+                        <div className="flex flex-wrap content-center justify-center">
+                            <p className="italic">DATA: 20 DE FEVEREIRO DE 2021</p>
+                        </div>
+                        <div></div>
+                        <div className="flex flex-wrap content-center justify-center">
+                            <p className="italic">HORÁRIO: 19:30</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="grid grid-rows-3 grid-flow-col justify-center"> {/*CORRIDA 2*/}
+                <div className="flex flex-wrap content-center justify-center border-r border-white">
+                    <img className="mt-3 flex flex-wrap content-center justify-center w-1/2"
+                        src="https://i.imgur.com/2XPKYG0.png" />
+                    <p className="italic">KARTÓDROMO DE INTERLAGOS</p>
+                </div>
+                <div className="flex flex-wrap content-center justify-center border-r border-white">
+                    <img className="mt-3 flex flex-wrap content-center justify-center w-3/4"
+                        src="https://i.imgur.com/y7hd09d.png" />
+                </div>
+                <div className="flex flex-wrap content-center justify-center border-r border-white rounded-b-3xl">
+                    <div className="grid grid-rows-3 grid-flow-col justify-center">
+                        <div className="flex flex-wrap content-center justify-center">
+                            <p className="italic">DATA: 28 DE MARÇO DE 2021</p>
+                        </div>
+                        <div></div>
+                        <div className="flex flex-wrap content-center justify-center">
+                            <p className="italic">HORÁRIO: 17:00</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="grid grid-rows-3 grid-flow-col justify-center"> {/*CORRIDA 3*/}
+                <div className="flex flex-wrap content-center justify-center border-r border-white">
+                    <img className="mt-3 flex flex-wrap content-center justify-center w-1/2"
+                        src="https://i.imgur.com/2XPKYG0.png" />
+                    <p className="italic">KARTÓDROMO DE INTERLAGOS</p>
+                </div>
+                <div className="flex flex-wrap content-center justify-center border-r border-white">
+                    <img className="mt-3 flex flex-wrap content-center justify-center w-3/4"
+                        src="https://i.imgur.com/y7hd09d.png" />
+                </div>
+                <div className="flex flex-wrap content-center justify-center border-r border-white rounded-b-3xl">
+                    <div className="grid grid-rows-3 grid-flow-col justify-center">
+                        <div className="flex flex-wrap content-center justify-center">
+                            <p className="italic">DATA: 18 DE ABRIL DE 2021</p>
+                        </div>
+                        <div></div>
+                        <div className="flex flex-wrap content-center justify-center">
+                            <p className="italic">HORÁRIO: 16:00</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="grid grid-rows-3 grid-flow-col justify-center"> {/*CORRIDA 4*/}
+                <div className="flex flex-wrap content-center justify-center border-r border-white">
+                    <img className="mt-3 flex flex-wrap content-center justify-center w-1/2"
+                        src="https://i.imgur.com/cuOVCoW.png" />
+                    <p className="italic">KARTÓDROMO GRANJA VIANA</p>
+                </div>
+                <div className="flex flex-wrap content-center justify-center border-r border-white">
+                    <img className="mt-3 flex flex-wrap content-center justify-center w-8/12"
+                        src="https://i.imgur.com/sbUq0Tl.png" />
+                </div>
+                <div className="flex flex-wrap content-center justify-center border-r border-white rounded-b-3xl">
+                    <div className="grid grid-rows-3 grid-flow-col justify-center">
+                        <div className="flex flex-wrap content-center justify-center">
+                            <p className="italic">DATA: 6 DE JUNHO DE 2021</p>
+                        </div>
+                        <div></div>
+                        <div className="flex flex-wrap content-center justify-center">
+                            <p className="italic">HORÁRIO: 20:30</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="grid grid-rows-3 grid-flow-col justify-center"> {/*CORRIDA 5*/}
+                <div className="flex flex-wrap content-center justify-center border-r border-white">
+                    <img className="mt-3 flex flex-wrap content-center justify-center"
+                        src="https://static.wixstatic.com/media/af30c3_82b173caa21a411584f9b33a5982edfb~mv2_d_3543_2189_s_2.png/v1/crop/x_0,y_155,w_3543,h_2034/fill/w_148,h_80,al_c,q_85,usm_1.20_1.00_0.01/logo-kart%C3%B3dromo-internacional-impress%C3%A3o_.webp" />
+                    <p className="italic">KARTÓDROMO ALDEIA DA SERRA</p>
+                </div>
+                <div className="flex flex-wrap content-center justify-center border-r border-white">
+                    <img className="mt-3 flex flex-wrap content-center justify-center w-7/12"
+                        src="https://i.imgur.com/7KjJ4Ta.png" />
+                </div>
+                <div className="flex flex-wrap content-center justify-center border-r border-white rounded-b-3xl">
+                    <div className="grid grid-rows-3 grid-flow-col justify-center">
+                        <div className="flex flex-wrap content-center justify-center">
+                            <p className="italic">DATA: 20 DE JUNHO DE 2021</p>
+                        </div>
+                        <div></div>
+                        <div className="flex flex-wrap content-center justify-center">
+                            <p className="italic">HORÁRIO: 15:00</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="grid grid-rows-3 grid-flow-col justify-center"> {/*CORRIDA 6*/}
+                <div className="flex flex-wrap content-center justify-center border-r border-white">
+                    <img className="mt-3 flex flex-wrap content-center justify-center w-1/2"
+                        src="https://i.imgur.com/cuOVCoW.png" />
+                    <p className="italic">KARTÓDROMO GRANJA VIANA</p>
+                </div>
+                <div className="flex flex-wrap content-center justify-center border-r border-white">
+                    <img className="mt-3 flex flex-wrap content-center justify-center w-8/12"
+                        src="https://i.imgur.com/sbUq0Tl.png" />
+                </div>
+                <div className="flex flex-wrap content-center justify-center border-r border-white rounded-b-3xl">
+                    <div className="grid grid-rows-3 grid-flow-col justify-center">
+                        <div className="flex flex-wrap content-center justify-center">
+                            <p className="italic">DATA: 10 DE JULHO DE 2021</p>
+                        </div>
+                        <div></div>
+                        <div className="flex flex-wrap content-center justify-center">
+                            <p className="italic">HORÁRIO: 18:30</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div >
     );
 }
 export default Races;
