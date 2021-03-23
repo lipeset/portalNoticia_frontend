b6fda27a7fe4c8ca013d04c1725ad37368f0b779
Felipe Lucas <felipea.lucas@outlook.com>
2021-03-23 12:27:22 -0300

    ADJUST

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,8 @@ const Noticia = ({ reverse, maxNews }) => {
     return (
         <Layout>
             <div className="container my-6 mx-auto px-4 md:px-12 md:grid grid-cols-2 grid-flow-col -mt-2">
-                <div className="container my-6 mx-auto md:px-12 border-red-500 border-t-8 border-r-8 rounded-r-3xl"> {/*notícia principal*/}
+                {/*NOTÍCIA PRINCIPAL*/}
+                <div className="container my-6 mx-auto md:px-12 border-red-500 border-t-8 border-r-8 rounded-r-3xl">
                     <div className="flex flex-wrap -mx-1 lg:-mx-10">
                         <div key={reverse[0]._id} className="my-1 px-1 w-full lg:my-4 lg:px-4">
                             <article className="transform hover:scale-105 transition duration-300 ease-in-out overflow-hidden rounded-lg shadow-lg">
@@ -39,10 +40,70 @@ const Noticia = ({ reverse, maxNews }) => {
                         </div>
                     </div>
                 </div>
-                <div className="container my-6 mx-auto px-4 md:px-12"> {/*demais notícias*/}
+                {/*DEMAIS NOTÍCIAS*/}
+                <div className="container my-6 mx-auto px-4 md:px-12"> 
                     <div className="flex flex-wrap -mx-1 lg:-mx-4">
-                        {maxNews.map(news => (
-                            <div key={news._id} className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2">
+                        {/*PENÚLTIMA NOTÍCIA*/}
+                        <div key={reverse[1]._id} className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2"> 
+                            <article className="transform hover:scale-105 transition duration-300 ease-in-out overflow-hidden rounded-lg shadow-lg
+                            border-red-500 border-b-2 border-l-2 rounded-l-3xl px-2">
+                                <Link href={`/noticias/${reverse[1]._id}`}>
+                                    <a className="no-underline hover:text-red-500 text-black">
+                                        <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                                            <h1 className="classA text-2xl">
+                                                {reverse[1].title}
+                                            </h1>
+                                        </header>
+                                        <img key={reverse[1]._id} className="flex flex-none" src={reverse[1].newsUrl} />
+                                    </a>
+                                </Link>
+                                <h3 className="flex items-center justify-between leading-tight p-2 md:p-4" >
+                                    {reverse[1].abstract}
+                                </h3>
+                                <footer className="flex items-center justify-between leading-tight p-2 md:p-4">
+                                    <Link href={`/pilotos/${reverse[1].author._id}`}>
+                                        <a className="classA flex items-center no-underline hover:text-red-500 hover:underline">
+                                            <img key={reverse[1].author._id} alt="Placeholder" className="block mr-2 rounded-full" src={reverse[1].author.avatarUrl} />
+                                            <p>Autor: {reverse[1].author.name}<br />
+                                                Data: {format(new Date(reverse[1].newsDate), 'dd/MM/yyyy')}
+                                            </p>
+                                        </a>
+                                    </Link>
+                                </footer>
+                            </article>
+                        </div>
+                        {/*ANTEPENÚLTIMA NOTÍCIA*/}
+                        <div key={reverse[2]._id} className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2"> 
+                            <article className="transform hover:scale-105 transition duration-300 ease-in-out overflow-hidden rounded-lg shadow-lg
+                            border-red-500 border-b-2 border-l-2 rounded-l-3xl px-2">
+                                <Link href={`/noticias/${reverse[2]._id}`}>
+                                    <a className="no-underline hover:text-red-500 text-black">
+                                        <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                                            <h1 className="classA text-2xl">
+                                                {reverse[2].title}
+                                            </h1>
+                                        </header>
+                                        <img key={reverse[2]._id} className="flex flex-none" src={reverse[2].newsUrl} />
+                                    </a>
+                                </Link>
+                                <h3 className="flex items-center justify-between leading-tight p-2 md:p-4" >
+                                    {reverse[2].abstract}
+                                </h3>
+                                <footer className="flex items-center justify-between leading-tight p-2 md:p-4">
+                                <Link href={`/pilotos/${reverse[2].author._id}`}>
+                                    <a className="classA flex items-center no-underline hover:text-red-500 hover:underline">
+                                        <img key={reverse[2].author._id} alt="Placeholder" className="block mr-2 rounded-full" src={reverse[2].author.avatarUrl} />
+                                        <p>Autor: {reverse[2].author.name}<br />
+                                            Data: {format(new Date(reverse[2].newsDate), 'dd/MM/yyyy')}
+                                        </p>
+                                    </a>
+                                </Link>
+                                </footer>
+                            </article>
+                        </div>                        
+                        {/*OUTRAS DUAS*/}
+                        {maxNews.map(news => ( 
+                            <div key={news._id} className="my-1 hidden md:block px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2">
                                 <article className="transform hover:scale-105 transition duration-300 ease-in-out overflow-hidden rounded-lg shadow-lg
                                 border-red-500 border-b-2 border-l-2 rounded-l-3xl px-2">
                                     <Link href={`/noticias/${news._id}`}>
@@ -73,8 +134,9 @@ const Noticia = ({ reverse, maxNews }) => {
                         ))}
                         <a href="/noticias">
                             <button className="flex items-center font-bold text-white bg-red-500 hover:bg-black py-1 px-3 focus:outline-none 
-                        rounded text-xl mt-4 md:mt-0 ">VER TODAS
-                            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                            rounded text-xl mt-4 md:mt-0 ">
+                                VER TODAS
+                                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                                 </svg>
                             </button>
@@ -89,8 +151,8 @@ const Noticia = ({ reverse, maxNews }) => {
 Noticia.getInitialProps = async () => {
     const { data: noticias } = await axios.get('https://portalnoticia-backend.herokuapp.com/news');
     const reverse = noticias.reverse();
-    const reverseSlice = reverse.slice(1);
-    const maxNews = reverseSlice.slice(0, 4);
+    const reverseSlice = reverse.slice(3);
+    const maxNews = reverseSlice.slice(0, 2);
 
     return {
         reverse,
