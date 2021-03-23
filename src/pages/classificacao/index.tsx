7e0ec19984c58dc673152f5749176ca2a0d10e14
Felipe Lucas <felipea.lucas@outlook.com>
2021-03-23 12:41:02 -0300

    add_routes

---
+++ src/pages/classificacao/index.tsx
@@ -0,0 +1,25 @@
+import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import React from 'react';
+import Layout from '../../components/Layout';
+
+export default function Piloto({ piloto }) {
+    return (
+        <Layout>
+            <div>
+                <h1>CLASSIFICAÇÃO FUNCIONANDO</h1>
+            </div>
+        </Layout>
+    )
+}
+export const getStaticProps: GetStaticProps = async () => {
+    const response = await fetch('https://portalnoticia-backend.herokuapp.com/pilots');
+    const data = await response.json();
+
+    return {
+        props: {
+            piloto: data,
+        },
+        revalidate: 1000
+    }
+};
