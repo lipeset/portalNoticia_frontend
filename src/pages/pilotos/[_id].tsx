7fac418d888f5cb2215a785474dba759d2868085
Felipe Lucas <felipea.lucas@outlook.com>
2021-03-30 15:38:47 -0300

    adjust

--- src/pages/pilotos/[_id].tsx
+++ src/pages/pilotos/[_id].tsx
@@ -10,34 +10,36 @@ export default function Piloto({ piloto }) {
 
     return (
         <Layout>
-            <div className="container my-4 mx-auto px-4 md:px-12 md:grid grid-cols-2 grid-flow-col border-black border-t-8 border-r-8 rounded-r-3xl shadow-lg">
-                <div>
-                    <img className="rounded-2xl my-4 md:w-11/12" src={piloto.profileUrl} />
-                    <h1 className="-mt-5 mb-4 text-5xl md:text-6xl">{piloto.name}</h1>
-                </div>
-                <div className="justify-between md:grid grid-rows-2">
-                    <div className="hidden md:flex justify-center">
-                        <img className="rounded-2xl my-4 w-3/5" src={piloto.team.gridUrl} />
+            <div className="container my-6 mx-auto px-4 md:px-12 md:grid grid-flow-col -mt-2">
+                <div className="container my-6 mx-auto px-1 md:px-3 border-black border-t-8 border-r-8 rounded-r-3xl shadow-lg md:grid grid-cols-2">
+                    <div className="ml-3 md:ml-0">
+                        <img className="rounded-2xl my-4 md:w-11/12" src={piloto.profileUrl} />
+                        <h1 className="-mt-5 mb-4 text-5xl md:text-6xl">{piloto.name}</h1>
                     </div>
-                    <div className="grid grid-cols-2 text-lg md:text-xl">
-                        <div className="font-bold">
-                            <h1>EQUIPE:</h1>
-                            <h1>PODIUNS:</h1>
-                            <h1>MELHOR CHEGADA:</h1>
-                            <h1>MELHOR LARGADA:</h1>
-                            <h1>POLE POSITIONS:</h1>
-                            <h1>VOLTAS RÁPIDAS:</h1>
-                            <h1>PESO:</h1>
+                    <div className="justify-between md:grid grid-rows-2">
+                        <div className="hidden md:flex justify-center">
+                            <img className="rounded-2xl my-4 w-3/5" src={piloto.team.gridUrl} />
                         </div>
-                        <div className="flex justify-end">
-                            <div>
-                                <h1>{piloto.team.alias}</h1>
-                                <h1>{piloto.podiuns}</h1>
-                                <h1>{piloto.highRaceFinish}</h1>
-                                <h1>{piloto.highGridPosition}</h1>
-                                <h1>{piloto.polePosition}</h1>
-                                <h1>{piloto.fastLaps}</h1>
-                                <h1>{piloto.weight} KG</h1>
+                        <div className="grid grid-cols-2 text-lg md:text-xl">
+                            <div className="font-bold ml-4">
+                                <h1>EQUIPE:</h1>
+                                <h1>PODIUNS:</h1>
+                                <h1>MELHOR CHEGADA:</h1>
+                                <h1>MELHOR LARGADA:</h1>
+                                <h1>POLE POSITIONS:</h1>
+                                <h1>VOLTAS RÁPIDAS:</h1>
+                                <h1>PESO:</h1>
+                            </div>
+                            <div className="flex justify-end mr-4">
+                                <div>
+                                    <h1>{piloto.team.alias}</h1>
+                                    <h1>{piloto.podiuns}</h1>
+                                    <h1>{piloto.highRaceFinish}</h1>
+                                    <h1>{piloto.highGridPosition}</h1>
+                                    <h1>{piloto.polePosition}</h1>
+                                    <h1>{piloto.fastLaps}</h1>
+                                    <h1>{piloto.weight} KG</h1>
+                                </div>
                             </div>
                         </div>
                     </div>
