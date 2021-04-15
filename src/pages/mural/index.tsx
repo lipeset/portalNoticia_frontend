860d6d3599c08e41f4c4ee1fa62a3a4bf55f3428
Felipe Lucas <felipea.lucas@outlook.com>
2021-04-15 18:57:22 -0300

    adjusts

--- src/pages/mural/index.tsx
+++ src/pages/mural/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Layout from '../../components/Layout';
 
 export default function Mural({ picUrl }) {
+    console.log(picUrl)
     return (
         <Layout>
             <div className="container my-6 mx-auto px-4 md:px-12 md:grid grid-flow-col -mt-2">
@@ -18,12 +19,12 @@ export default function Mural({ picUrl }) {
                         </p>
                     </div>
                     <div className="container my-6 mx-auto px-4 md:px-12 md:grid grid-cols-4 -mt-2 items-center">
-                        {picUrl[0].muralPics.reverse().map((pics) => (
+                        {picUrl.reverse().map((pics) => (
                             <div key={pics} className="my-1 px-1 w-full lg:my-4 lg:px-4 container md:px-12 flex-wrap -mx-1 lg:-mx-4">
                                 <article className="transform hover:scale-105 transition duration-300 ease-in-out overflow-hidden rounded-lg shadow-lg
                                 border-black border-t-2 border-r-2 rounded-r-3xl px-2">
                                     <a className="no-underline">
-                                        <img key={pics._id} className="flex flex-none" src={pics} />
+                                        <img key={pics._id} className="flex flex-none" src={pics.muralPics} />
                                     </a>
                                 </article>
                             </div>
