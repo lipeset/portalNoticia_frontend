461efccd9c137a96287dd56cbe9069abd5eb4fc4
Felipe Lucas <felipea.lucas@outlook.com>
2021-04-15 18:57:53 -0300

    adjusts

--- src/pages/mural/index.tsx
+++ src/pages/mural/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Layout from '../../components/Layout';
 
 export default function Mural({ picUrl }) {
-    console.log(picUrl)
     return (
         <Layout>
             <div className="container my-6 mx-auto px-4 md:px-12 md:grid grid-flow-col -mt-2">
