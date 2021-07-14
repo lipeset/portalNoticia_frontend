e8c24fa7de2c72206a1ead8f20c556bd53c618aa
Felipe Lucas <felipea.lucas@outlook.com>
2021-07-14 16:24:27 -0300

    adsjusts

--- src/pages/equipes/[_id].tsx
+++ src/pages/equipes/[_id].tsx
@@ -53,50 +53,89 @@ export default function Equipe({ equipe }) {
                             <div className="font-bold">
                                 <h1>NOME CURTO:</h1>
                                 <h1>TÍTULOS:</h1>
-                                <h1>PONTOS 1º GP:</h1>
-                                <h1>MP 1º GP:</h1>
-                                <h1>PONTOS 2º GP:</h1>
-                                <h1>MP 2º GP:</h1>
-                                <h1>PONTOS 3º GP:</h1>
-                                <h1>MP 3º GP:</h1>
-                                <h1>PONTOS 4º GP:</h1>
-                                <h1>MP 4º GP:</h1>
-                                <h1>PONTOS 5º GP:</h1>
-                                <h1>MP 5º GP:</h1>
-                                <h1>PONTOS 6º GP:</h1>
-                                <h1>MP 6º GP:</h1>
                                 <h1>MELHOR POSIÇÃO:</h1>
                                 <h1>POLE POSITIONS:</h1>
                                 <h1>VOLTAS RÁPIDAS:</h1>
-                                <h1>MV ALDEIA:</h1>
-                                <h1>MV SAN MARINO:</h1>
-                                <h1>MV GRANJA VIANA:</h1>
-                                <h1>MV INTERLAGOS:</h1>
                             </div>
                             <div className="flex justify-end">
                                 <div className="text-center">
                                     <h1>{equipe.alias}</h1>
                                     <h1>{equipe.champCamp}</h1>
-                                    <h1>{equipe.firstGpPoints}</h1>
-                                    <h1>{equipe.firstGpFinish}</h1>
-                                    <h1>{equipe.secondGpPoints}</h1>
-                                    <h1>{equipe.secondGpFinish}</h1>
-                                    <h1>{equipe.thirdGpPoints}</h1>
-                                    <h1>{equipe.thirdGpFinish}</h1>
-                                    <h1>{equipe.fourthGpPoints}</h1>
-                                    <h1>{equipe.fourthGpFinish}</h1>
-                                    <h1>{equipe.fifithGpPoints}</h1>
-                                    <h1>{equipe.fifithGpFinish}</h1>
-                                    <h1>{equipe.sixthGpPoints}</h1>
-                                    <h1>{equipe.sixthGpFinish}</h1>
                                     <h1>{equipe.teamHighRaceFinish}</h1>
                                     <h1>{equipe.teamPolePositions}</h1>
                                     <h1>{equipe.teamFastLaps}</h1>
-                                    <h1>{equipe.bestAldeiaLap}</h1>
-                                    <h1>{equipe.bestSanMarinoLap}</h1>
-                                    <h1>{equipe.bestKgvLap}</h1>
-                                    <h1>{equipe.bestInterlagosLap}</h1>
                                 </div>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-4 bg-pink-400">
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">MV Aldeia: </p>
+                                <p className="text-xl md:text-2xl">{equipe.bestAldeiaLap}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <label className="text-xl md:text-2xl">MV G. Viana: </label>
+                                <p className="text-xl md:text-2xl">{equipe.bestKgvLap}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <label className="text-xl md:text-2xl">MV Interlagos: </label>
+                                <p className="text-xl md:text-2xl">{equipe.bestInterlagosLap}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <label className="text-xl md:text-2xl">MV S. Marino: </label>
+                                <h1 className="text-xl md:text-2xl">{equipe.bestSanMarinoLap}</h1>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-6 bg-green-300">
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pontos 1ª etapa: </p>
+                                <p className="text-xl md:text-2xl">{equipe.firstGpPoints}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pontos 2ª etapa: </p>
+                                <p className="text-xl md:text-2xl">{equipe.secondGpPoints}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pontos 3ª etapa: </p>
+                                <p className="text-xl md:text-2xl">{equipe.thirdGpPoints}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pontos 4ª etapa: </p>
+                                <p className="text-xl md:text-2xl">{equipe.fourthGpPoints}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pontos 5ª etapa: </p>
+                                <p className="text-xl md:text-2xl">{equipe.fifithGpPoints}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pontos 6ª etapa: </p>
+                                <p className="text-xl md:text-2xl">{equipe.sixthGpPoints}</p>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-6 bg-green-400">
+
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">MP: </p>
+                                <p className="text-xl md:text-2xl">{equipe.firstGpFinish}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">MP:</p>
+                                <p className="text-xl md:text-2xl">{equipe.secondGpFinish}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">MP:</p>
+                                <p className="text-xl md:text-2xl">{equipe.thirdGpFinish}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">MP:</p>
+                                <p className="text-xl md:text-2xl">{equipe.fourthGpFinish}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">MP:</p>
+                                <p className="text-xl md:text-2xl">{equipe.fifithGpFinish}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">MP:</p>
+                                <p className="text-xl md:text-2xl">{equipe.sixthGpFinish}</p>
                             </div>
                         </div>
                         <div className="p-1 text-lg">
--- src/pages/pilotos/[_id].tsx
+++ src/pages/pilotos/[_id].tsx
@@ -22,47 +22,99 @@ export default function Piloto({ piloto }) {
                         <div className="grid grid-cols-2 text-lg md:text-2xl">
                             <div className="font-bold">
                                 <h1>EQUIPE:</h1>
+                                <h1>TÍTULOS:</h1>
                                 <h1>PODIUNS:</h1>
-                                <h1>PONTOS 1º GP:</h1>
-                                <h1>PONTOS 2º GP:</h1>
-                                <h1>PONTOS 3º GP:</h1>
-                                <h1>PONTOS 4º GP:</h1>
-                                <h1>PONTOS 5º GP:</h1>
-                                <h1>PONTOS 6º GP:</h1>
                                 <h1>MELHOR CHEGADA:</h1>
                                 <h1>MELHOR LARGADA:</h1>
                                 <h1>POLE POSITIONS:</h1>
                                 <h1>VOLTAS RÁPIDAS:</h1>
-                                <h1>MV ALDEIA:</h1>
-                                <h1>MV SAN MARINO:</h1>
-                                <h1>MV GRANJA VIANA:</h1>
-                                <h1>MV INTERLAGOS:</h1>
-                                <h1>PESO:</h1><br />
+                                <h1>PESO:</h1>
                             </div>
                             <div className="flex justify-end">
                                 <div className="text-center">
                                     <h1>{piloto.team.alias}</h1>
+                                    <h1>{piloto.champCamp}</h1>
                                     <h1>{piloto.podiuns}</h1>
-                                    <h1>{piloto.firstGpPoints}</h1>
-                                    <h1>{piloto.secondGpPoints}</h1>
-                                    <h1>{piloto.thirdGpPoints}</h1>
-                                    <h1>{piloto.fourthGpPoints}</h1>
-                                    <h1>{piloto.fifithGpPoints}</h1>
-                                    <h1>{piloto.sixthGpPoints}</h1>
                                     <h1>{piloto.highRaceFinish}</h1>
                                     <h1>{piloto.highGridPosition}</h1>
                                     <h1>{piloto.polePosition}</h1>
                                     <h1>{piloto.fastLaps}</h1>
-                                    <h1>{piloto.bestAldeiaLap}</h1>
-                                    <h1>{piloto.bestSanMarinoLap}</h1>
-                                    <h1>{piloto.bestKgvLap}</h1>
-                                    <h1>{piloto.bestInterlagosLap}</h1>
                                     <h1>{piloto.weight} KG</h1>
                                 </div>
                             </div>
                         </div>
+                        <div className="grid grid-cols-4 bg-pink-400">
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">MV Aldeia: </p>
+                                <p className="text-xl md:text-2xl">{piloto.bestAldeiaLap}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <label className="text-xl md:text-2xl">MV G. Viana: </label>
+                                <p className="text-xl md:text-2xl">{piloto.bestKgvLap}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <label className="text-xl md:text-2xl">MV Interlagos: </label>
+                                <p className="text-xl md:text-2xl">{piloto.bestInterlagosLap}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <label className="text-xl md:text-2xl">MV S. Marino: </label>
+                                <h1 className="text-xl md:text-2xl">{piloto.bestSanMarinoLap}</h1>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-6 bg-green-300">
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pontos 1ª etapa: </p>
+                                <p className="text-xl md:text-2xl">{piloto.firstGpPoints}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pontos 2ª etapa: </p>
+                                <p className="text-xl md:text-2xl">{piloto.secondGpPoints}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pontos 3ª etapa: </p>
+                                <p className="text-xl md:text-2xl">{piloto.thirdGpPoints}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pontos 4ª etapa: </p>
+                                <p className="text-xl md:text-2xl">{piloto.fourthGpPoints}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pontos 5ª etapa: </p>
+                                <p className="text-xl md:text-2xl">{piloto.fifithGpPoints}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pontos 6ª etapa: </p>
+                                <p className="text-xl md:text-2xl">{piloto.sixthGpPoints}</p>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-6 bg-green-400">
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pos.: </p>
+                                <p className="text-xl md:text-2xl">{piloto.firstGpFinish}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pos.:</p>
+                                <p className="text-xl md:text-2xl">{piloto.secondGpFinish}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pos.:</p>
+                                <p className="text-xl md:text-2xl">{piloto.thirdGpFinish}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pos.:</p>
+                                <p className="text-xl md:text-2xl">{piloto.fourthGpFinish}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pos.:</p>
+                                <p className="text-xl md:text-2xl">{piloto.fifithGpFinish}</p>
+                            </div>
+                            <div className="border-2 border-black text-center">
+                                <p className="text-xl md:text-2xl">Pos.:</p>
+                                <p className="text-xl md:text-2xl">{piloto.sixthGpFinish}</p>
+                            </div>
+                        </div>
                         <div className="p-1 text-lg">
-                            <h1>MV = Melhor volta</h1>
+                            <h1>MV = Melhor volta // POS = Posição</h1>
                         </div>
                     </div>
                 </div>
